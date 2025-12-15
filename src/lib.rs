40572777f7fe58fa9f854cfdcabd0d273aac8d19
Thorsten Deinert <thorsten.deinert@uni-dortmund.de>
2025-12-15 18:15:04 +0100

    Define first test - does not compile

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,12 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_multiplication() {
+        let five = Dollar::new(5);
+        five.times(2);
+        assert_eq!(10, five.amount);
     }
 }
