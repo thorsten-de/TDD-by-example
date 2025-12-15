37b5ba688c617b61b7a560a5a26af73afddb111b
Thorsten Deinert <thorsten.deinert@uni-dortmund.de>
2025-12-15 19:56:10 +0100

    Refactor times to use immutable access to self. No need to change state anymore.

--- the-money-example/src/lib.rs
+++ the-money-example/src/lib.rs
@@ -18,7 +18,7 @@ mod tests {
 
     #[test]
     fn test_multiplication() {
-        let mut five = Dollar::new(5);
+        let five = Dollar::new(5);
 
         let product: Dollar = five.times(2);
         assert_eq!(10, product.amount);
