3add1e0dacbbfa3d13309a44d336f32e5e30441f
Thorsten Deinert <thorsten.deinert@uni-dortmund.de>
2025-12-17 16:23:18 +0100

    1.6 Refactoring: Move calculation to method

--- the-money-example-csharp/Dollar.cs
+++ the-money-example-csharp/Dollar.cs
@@ -2,13 +2,14 @@ namespace Tdd.Money;
 
 public class Dollar
 {
-    public int Amount { get; private set; } = 2 * 5;
+    public int Amount { get; private set; }
     
     public Dollar(int amount)
     {
     }
 
     public void Times(int times)
-    {
+    { 
+        Amount = 2 * 5;
     }
 }
