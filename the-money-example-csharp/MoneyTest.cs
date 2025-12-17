fdd4b4065eb3c895faac0b79f9e3212dce533cb7
Thorsten Deinert <thorsten.deinert@uni-dortmund.de>
2025-12-17 22:23:46 +0100

    2.1 Change Test to impossible side-effect version

--- the-money-example-csharp/MoneyTest.cs
+++ the-money-example-csharp/MoneyTest.cs
@@ -3,10 +3,13 @@
 public class MoneyTest
 {
     [Fact]
-    public void TestMultiplicaton()
+    public void TestMultiplication()
     {
         Dollar five = new Dollar(5);
         five.Times(2);
         Assert.Equal(10, five.Amount);
+        
+        five.Times(3);
+        Assert.Equal(15, five.Amount);
     }
 }
