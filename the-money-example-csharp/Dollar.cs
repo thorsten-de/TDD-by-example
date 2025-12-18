f360c3d9ed3ac23df9f68217cf7ea6eccba27a6d
Thorsten Deinert <thorsten.deinert@uni-dortmund.de>
2025-12-18 08:25:59 +0100

    9.3 Reconcile Currency implementation in Dollar and Franc

--- the-money-example-csharp/Dollar.cs
+++ the-money-example-csharp/Dollar.cs
@@ -2,12 +2,15 @@ namespace Tdd.Money;
 
 public class Dollar: Money
 {
+    private readonly string _currency;
+    
     public Dollar(int amount) :  base(amount)
     {
+        _currency = "USD";
     }
 
     public override Money Times(int multiplier) =>
         new Dollar(_amount * multiplier);
     
-    public override string Currency => "USD";
+    public override string Currency => _currency;
 }
--- the-money-example-csharp/Franc.cs
+++ the-money-example-csharp/Franc.cs
@@ -2,12 +2,15 @@ namespace Tdd.Money;
 
 public class Franc: Money
 {
+    private readonly string _currency;
+    
     public Franc(int amount) :  base(amount)
     {
+        _currency = "CHF";
     }
 
     public override Money Times(int multiplier) =>
         new Franc(_amount * multiplier);
 
-    public override string Currency => "CHF";
+    public override string Currency => _currency;
 }
