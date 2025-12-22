bafba1e068f0ebe42e8b2505cb365adf89f6a9d6
Thorsten Deinert <thorsten.deinert@uni-dortmund.de>
2025-12-22 23:09:59 +0100

    14.7 Refactor: Store rates inside Bank - fails!?

--- the-money-example-csharp/Bank.cs
+++ the-money-example-csharp/Bank.cs
@@ -2,16 +2,14 @@ namespace Tdd.Money;
 
 public class Bank
 {
+    private Dictionary<(string, string), int> _rates = new();
     public Money Reduce(IExpression source, string to) => source.Reduce(this, to);
 
     public void AddRate(string from, string to, int rate)
     {
+        _rates.Add((from, to), rate);
     }
 
-    public int Rate(string from, string to) =>
-        (from, to) switch
-        {
-            (from: "CHF", to: "USD") => 2,
-            _ => 1
-        };
+    public int Rate(string from, string to) => 
+        _rates.GetValueOrDefault((from, to));
 }
