a35e81a2128afd3fc38c82bae9fb15d6091f9194
Thorsten Deinert <thorsten.deinert@uni-dortmund.de>
2025-12-22 19:28:01 +0100

    13.8 Switch on expression type to make reduce test pass.

--- the-money-example-csharp/Bank.cs
+++ the-money-example-csharp/Bank.cs
@@ -4,7 +4,14 @@ public class Bank
 {
     public Money Reduce(IExpression source, string to)
     {
-        Sum sum = (Sum)source;
-        return sum.Reduce(to);
+        switch (source)
+        {
+            case Money money:
+                return money;
+            case Sum sum:
+                return sum.Reduce(to);
+            default:
+                throw new NotImplementedException();
+        }
     }
 }
