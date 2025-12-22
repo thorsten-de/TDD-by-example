759b8dd4fd5340a020dd6258d230b409640cd9d3
Thorsten Deinert <thorsten.deinert@uni-dortmund.de>
2025-12-22 22:45:37 +0100

    14.2 Make test compile and fail

--- the-money-example-csharp/Bank.cs
+++ the-money-example-csharp/Bank.cs
@@ -3,4 +3,8 @@ namespace Tdd.Money;
 public class Bank
 {
     public Money Reduce(IExpression source, string to) => source.Reduce(to);
+
+    public void AddRate(string from, string to, int rate)
+    {
+    }
 }
