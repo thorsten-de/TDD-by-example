5e242b626c6e267cf9d6bd58f07f76673a1ad81a
Thorsten Deinert <thorsten.deinert@uni-dortmund.de>
2025-12-22 23:37:48 +0100

    16.1 Add a failing test for Sum.Plus

--- the-money-example-csharp/MoneyTest.cs
+++ the-money-example-csharp/MoneyTest.cs
@@ -104,12 +104,24 @@ public class MoneyTest
     {
         IExpression fiveBucks = Money.Dollar(5);
         IExpression tenFrancs = Money.Franc(10);
-        
         Bank bank = new Bank();
         bank.AddRate("CHF", "USD", 2);
 
         Money result = bank.Reduce(fiveBucks.Plus(tenFrancs), "USD");
         Assert.Equal(Money.Dollar(10), result);
+    }
+
+    [Fact]
+    public void TestSumPlusMoney()
+    {
+        IExpression fiveBucks = Money.Dollar(5);
+        IExpression tenFrancs = Money.Franc(10);
+        Bank bank = new Bank();
+        bank.AddRate("CHF", "USD", 2);
+        
+        IExpression sum = new Sum(fiveBucks, tenFrancs).Plus(fiveBucks);
+        Money result = bank.Reduce(sum, "USD");
+        Assert.Equal(Money.Dollar(15), result);
     }
     
 }
