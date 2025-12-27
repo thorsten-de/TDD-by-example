cd02a329db6980f9240845a7b4be596c9dbe4f6d
Thorsten Deinert <thorsten.deinert@uni-dortmund.de>
2025-12-27 22:22:46 +0100

    20.3 Add log string for testMethod and assert on log

--- the-xUnit-example/myUnit.py
+++ the-xUnit-example/myUnit.py
@@ -16,6 +16,7 @@ class WasRun(TestCase):
 
     def testMethod(self):
         self.wasRun = 1
+        self.log = self.log + "testMethod "
 
     def setUp(self):
         self.wasRun = None
@@ -31,7 +32,7 @@ class TestCaseTest(TestCase):
 
     def testSetUp(self):
         self.test.run()
-        assert("setUp " ==  self.test.log)
+        assert("setUp testMethod " ==  self.test.log)
 
 TestCaseTest("testRunning").run()
 TestCaseTest("testSetUp").run()
