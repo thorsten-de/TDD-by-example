8f94d333f64441e7deaf1fabad9416e57d18a3bf
Thorsten Deinert <thorsten.deinert@uni-dortmund.de>
2025-12-27 22:10:18 +0100

    19.3 Refactor WasRun  to use setUp
    - Initialize wasRun in setUp
    - Simplify testRunning to not assert the initialized value of wasRun.
    
    SetUp is tested in a seperate test and we are confident about it

--- the-xUnit-example/myUnit.py
+++ the-xUnit-example/myUnit.py
@@ -12,19 +12,18 @@ class TestCase:
 
 class WasRun(TestCase):
     def __init__(self, name):
-        self.wasRun = None
         TestCase.__init__(self, name)
 
     def testMethod(self):
         self.wasRun = 1
 
     def setUp(self):
+        self.wasRun = None
         self.wasSetUp = 1
 
 class TestCaseTest(TestCase):
     def testRunning(self):
         test = WasRun("testMethod")
-        assert(not test.wasRun)
         test.run()
         assert(test.wasRun)
 
