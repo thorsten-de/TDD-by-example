87d9b3dfae69d43479bc3e0e3c1f07ebdaeac593
Thorsten Deinert <thorsten.deinert@uni-dortmund.de>
2025-12-27 22:30:37 +0100

    20.6 Add assertion for tearDown called

--- the-xUnit-example/myUnit.py
+++ the-xUnit-example/myUnit.py
@@ -24,7 +24,7 @@ class TestCaseTest(TestCase):
     def testTemplateMethod(self):
         self.test = WasRun("testMethod")
         self.test.run()
-        assert("setUp testMethod " ==  self.test.log)
+        assert("setUp testMethod tearDown " ==  self.test.log)
 
 TestCaseTest("testTemplateMethod").run()
        
