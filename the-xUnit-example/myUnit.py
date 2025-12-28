0ee9a9fdf4ae4079c429cd7e94f7d0f003188e6f
Thorsten Deinert <thorsten.deinert@uni-dortmund.de>
2025-12-28 09:44:44 +0100

    22.2 Make test pass by counting failed tests

--- the-xUnit-example/myUnit.py
+++ the-xUnit-example/myUnit.py
@@ -1,12 +1,16 @@
 class TestResult:
     def __init__(self):
         self.runCount = 0
+        self.errorCount = 0
 
     def testStarted(self):
         self.runCount += 1
 
+    def testFailed(self):
+        self.errorCount += 1
+
     def summary(self):
-        return f"{self.runCount} run, 0 failed"
+        return f"{self.runCount} run, {self.errorCount} failed"
     
 class TestCase:
     def __init__(self, name):
