df7608039a6b16bebd8e1c449f5836dedfaf91ab
Thorsten Deinert <thorsten.deinert@uni-dortmund.de>
2025-12-28 09:35:36 +0100

    21.4 Refactor to real test counting

--- the-xUnit-example/myUnit.py
+++ the-xUnit-example/myUnit.py
@@ -1,19 +1,25 @@
 class TestResult:
     def __init__(self):
-        self.runCount = 1
+        self.runCount = 0
+
+    def testStarted(self):
+        self.runCount += 1
 
     def summary(self):
         return f"{self.runCount} run, 0 failed"
+    
 class TestCase:
     def __init__(self, name):
         self.name = name
 
     def run(self):
+        result = TestResult()
+        result.testStarted()
         self.setUp()
         method = getattr(self, self.name)
         method()
         self.tearDown()
-        return TestResult()
+        return result
 
     def setUp(self):
         pass
